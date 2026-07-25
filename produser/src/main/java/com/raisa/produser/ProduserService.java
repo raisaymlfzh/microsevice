588bb9f812084aa81007f6e59998bb8a61e21338
raisaymlfzh <rymlfzh@gmail.com>
2026-07-25 20:18:02 +0700

    Add files via upload

---
+++ produser/src/main/java/com/raisa/produser/ProduserService.java
@@ -0,0 +1,23 @@
+package com.raisa.produser;
+
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProduserService {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    private Queue queue;
+
+    public void sendMessage(String message){
+        rabbitTemplate.convertAndSend(queue.getName(), message);
+        System.out.println("sent:" +message);
+    }
+
+
+}
